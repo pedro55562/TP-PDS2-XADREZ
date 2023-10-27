df807acbdd6c7df1977dea8edc1bc24044a0b9e5
pedro55562 <pedro55562@ufmg.br>
2023-10-27 00:18:15 -0300

    adicionando contrato de Bishop.h

--- include/Bishop.h
+++ include/Bishop.h
@@ -1,11 +1,38 @@
+/**
+ * @file Bishop.h
+ * @brief Define a classe Bishop para representar uma peça de bispo no jogo de xadrez.
+ */
+
 #ifndef BISHOP_H
 #define BISHOP_H
 
-class Bishop {
-    
-    private:
+#include <cmath>
 
-    public:
+#include "Chess_constants.h"
+#include "Piece.h"
+
+/**
+ * @class Bishop
+ * @brief Classe que representa uma peça de bispo no jogo de xadrez. Herda da classe Piece.
+ */
+class Bishop : public Piece {
+
+public:
+    /**
+     * @brief Construtor da classe Bishop.
+     * @param BishopColor A cor da peça de bispo (WHITE ou BLACK).
+     */
+    Bishop(int BishopColor);
+
+    /**
+     * @brief Verifica se um movimento é válido para o bispo.
+     * @param fromX A coordenada X da posição de origem.
+     * @param fromY A coordenada Y da posição de origem.
+     * @param toX A coordenada X da posição de destino.
+     * @param toY A coordenada Y da posição de destino.
+     * @return true se o movimento for válido, false caso contrário.
+     */
+    bool isValidBishopMove(int fromX, int fromY, int toX, int toY) const;
 };
 
-#endif 
+#endif
