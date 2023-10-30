241b93e455a0502ba2b81b94d217966441b1fd8e
pedro55562 <pedro55562@ufmg.br>
2023-10-30 18:03:58 -0300

    adicionando contrato da classe Knight

--- include/Knight.h
+++ include/Knight.h
@@ -1,12 +1,37 @@
+/**
+ * @file Knight.h
+ * @brief Define a classe Knight para representar uma peça de cavaleiro no jogo de xadrez.
+ */
+
 #ifndef KNIGHT_H
 #define KNIGHT_H
 
-class Knight  {
+#include <cmath>
 
-    private:
+#include "Piece.h"
+#include "Chess_constants.h"
 
-    public:
-    
+/**
+ * @class Knight
+ * @brief Classe que representa uma peça de cavaleiro no jogo de xadrez. Herda da classe Piece.
+ */
+class Knight : public Piece {
+public:
+    /**
+     * @brief Construtor da classe Knight.
+     * @param KingColor A cor da peça de cavaleiro (WHITE ou BLACK).
+     */
+    Knight(int KingColor);
+
+    /**
+     * @brief Verifica se um movimento é válido para o cavaleiro.
+     * @param fromX A coordenada X da posição de origem.
+     * @param fromY A coordenada Y da posição de origem.
+     * @param toX A coordenada X da posição de destino.
+     * @param toY A coordenada Y da posição de destino.
+     * @return true se o movimento for válido, false caso contrário.
+     */
+    bool isValidKnightMove(int fromX, int fromY, int toX, int toY) const;
 };
 
 #endif
