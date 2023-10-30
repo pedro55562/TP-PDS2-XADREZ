ba43aa7ab1ac9bcf3ef83cc3a21d0af74ae2d30c
pedro55562 <pedro55562@ufmg.br>
2023-10-30 18:20:16 -0300

    adiconando contrato Queen.h

--- include/Queen.h
+++ include/Queen.h
@@ -1,10 +1,35 @@
+/**
+ * @file Queen.h
+ * @brief Define a classe Queen para representar uma peça de rainha no jogo de xadrez.
+ */
+
 #ifndef QUEEN_H
 #define QUEEN_H
 
-class Queen {
-    private:
+#include "Chess_constants.h"
+#include "Piece.h"
 
-    public:
+/**
+ * @class Queen
+ * @brief Classe que representa uma peça de rainha no jogo de xadrez. Herda da classe Piece.
+ */
+class Queen : public Piece {
+public:
+    /**
+     * @brief Construtor da classe Queen.
+     * @param QueenColor A cor da peça de rainha (WHITE ou BLACK).
+     */
+    Queen(int QueenColor);
+
+    /**
+     * @brief Verifica se um movimento é válido para a rainha.
+     * @param fromX A coordenada X da posição de origem.
+     * @param fromY A coordenada Y da posição de origem.
+     * @param toX A coordenada X da posição de destino.
+     * @param toY A coordenada Y da posição de destino.
+     * @return true se o movimento for válido, false caso contrário.
+     */
+    bool isValidQueenMove(int fromX, int fromY, int toX, int toY) const;
 };
 
 #endif
