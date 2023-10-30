1ffdb08bd7fd8eb8ea638b60af70577cc8a9fa37
pedro55562 <pedro55562@ufmg.br>
2023-10-30 17:47:34 -0300

    adicionando contrato de king.h

--- include/King.h
+++ include/King.h
@@ -1,12 +1,35 @@
+/**
+ * @file King.h
+ * @brief Define a classe King para representar uma peça de rei no jogo de xadrez.
+ */
+
 #ifndef KING_H
 #define KING_H
 
-class King {
+#include "Piece.h"
+#include "Chess_constants.h"
 
-    private:
+/**
+ * @class King
+ * @brief Classe que representa uma peça de rei no jogo de xadrez. Herda da classe Piece.
+ */
+class King : public Piece {
+public:
+    /**
+     * @brief Construtor da classe King.
+     * @param KingColor A cor da peça de rei (WHITE ou BLACK).
+     */
+    King(int KingColor);
 
-    public:
-    
+    /**
+     * @brief Verifica se um movimento é válido para o rei.
+     * @param fromX A coordenada X da posição de origem.
+     * @param fromY A coordenada Y da posição de origem.
+     * @param toX A coordenada X da posição de destino.
+     * @param toY A coordenada Y da posição de destino.
+     * @return true se o movimento for válido, false caso contrário.
+     */
+    bool isValidKingMove(int fromX, int fromY, int toX, int toY) const;
 };
 
 #endif
